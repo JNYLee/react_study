168fa6568a3b7ec68c67cf7a73c035a588e30fcf
JNYLee <junyjuny1021@gmail.com>
2021-11-22 18:27:26 +0900

    [reactSpeed/init]: 반응속도 UI만들기

--- reactSpeed/ResponseCheck.jsx
+++ reactSpeed/ResponseCheck.jsx
@@ -6,28 +6,58 @@ class ResponseCheck extends Component {
     message: "클릭해서 시작하세요.",
     result: [],
   };
-
-  onClickScreen = () => {};
+  timeout;
+  startTime;
+  endTime;
+  onClickScreen = () => {
+    const { state, message, result } = this.state;
+    if (state === "waiting") {
+      this.setState({
+        state: "ready",
+        message: "초록색이 되면 클릭하세요",
+      });
+      this.timeout = setTimeout(() => {
+        this.setState({
+          state: "now",
+          message: "지금 클릭",
+        });
+        this.startTime = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000);
+    } else if (state === "ready") {
+      clearTimeout(this.timeout);
+      this.setState({
+        state: "waiting",
+        message: "너무 성급하시군요! 초록색이 된 후에 클릭하세요.",
+      });
+    } else if (state === "now") {
+      this.endTime = new Date();
+      this.setState((prevState) => {
+        return {
+          state: "waiting",
+          message: "클릭해서 시작하세요",
+          result: [...prevState.result, this.endTime - this.startTime],
+        };
+      });
+    }
+  };
 
   renderAverage = () => {
-    return this.state.result.length === 0 ? null : (
+    const { result } = this.state;
+    return result.length === 0 ? null : (
       <div>
         평균 시간:
-        {this.state.result.reduce((a, c) => a + c) / this.state.result.length}
+        {result.reduce((a, c) => a + c) / result.length}
         ms
       </div>
     );
   };
 
   render() {
+    const { state, message, result } = this.state;
     return (
       <>
-        <div
-          id="screen"
-          className={this.state.state}
-          onClick={this.onClickScreen}
-        >
-          {this.state.message}
+        <div id="screen" className={state} onClick={this.onClickScreen}>
+          {message}
         </div>
         <div>{this.renderAverage()}</div>
       </>
