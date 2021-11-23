92a181dab2da9ff63c6cb40ab44ca749e72a5b78
JNYLee <junyjuny1021@gmail.com>
2021-11-23 12:42:13 +0900

    [rspGame/init]: hooks로 만들어보기

--- rspGame/RSP.jsx
+++ rspGame/RSP.jsx
@@ -1,15 +1,10 @@
 import React, { Component } from "react";
 
-// 클래스의 경우 -> constructor -> render -> ref -> componentDidMount
-// (setState/props 바뀔때) -> shouldComponentUpdate(true) -> render -> componentDidUpdate
-// 부모가 나를 없앴을 때 -> componentWillUnmount -> 소멸
-
 const rspCoords = {
   바위: "0",
   가위: "-142px",
   보: "-284px",
 };
-
 const scores = {
   가위: 1,
   바위: 0,
@@ -25,22 +20,14 @@ const computerChoice = (imgCoord) => {
 class RSP extends Component {
   state = {
     result: "",
-    imgCoord: rspCoords.바위,
+    imgCoord: "0",
     score: 0,
   };
 
   interval;
-
   componentDidMount() {
-    // 컴포넌트가 첫 렌더링된 후, 여기에 비동기 요청을 많이 해요
-    this.interval = setInterval(this.changeHand, 100);
+    this.interval = setInterval(this.changeHand, 3000);
   }
-
-  componentWillUnmount() {
-    // 컴포넌트가 제거되기 직전, 비동기 요청 정리를 많이 해요
-    clearInterval(this.interval);
-  }
-
   changeHand = () => {
     const { imgCoord } = this.state;
     if (imgCoord === rspCoords.바위) {
@@ -84,9 +71,13 @@ class RSP extends Component {
       });
     }
     setTimeout(() => {
-      this.interval = setInterval(this.changeHand, 100);
-    }, 1000);
+      this.interval = setInterval(this.changeHand, 1000);
+    }, 2000);
   };
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
   render() {
     const { result, score, imgCoord } = this.state;
@@ -98,8 +89,10 @@ class RSP extends Component {
             background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`,
           }}
         />
+        {console.log("rsp is rerendering")}
         <div>
           <button id="rock" className="btn" onClick={this.onClickBtn("바위")}>
+            {console.log("Button rerendering!!!")}
             바위
           </button>
           <button
@@ -114,10 +107,12 @@ class RSP extends Component {
           </button>
         </div>
         <div>{result}</div>
-        <div>현재 {score}점</div>
+        <div>
+          현재 {score}점 {console.log("score rerendering!!!")}
+        </div>
       </>
     );
   }
 }
 
-export default RSP;
+export default RSP2;
